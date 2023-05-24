47aa44f6ceccf05b072e11ee711de2ee65f35017
PedroYouse <pedro.oliveira@youse.com.br>
2023-05-24 10:18:43 -0300

    Helper data/hora

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,9 @@
 module WelcomeHelper
+    def date_br ()
+        data_us = Time.new.strftime("%d/%m/%Y") 
+    end
+
+    def time_br ()
+        time_us = Time.new.strftime("%H:%M:%S")
+    end
 end
